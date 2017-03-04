0b19a6f1a897a3f98f2e68ce68e6aa6b514e5fc4
Łukasz Wasiluk <galawanty@gmail.com>
2017-03-04 01:46:50 +0100

    html js

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+var triangle1Area = getTriangleArea(5, -10);
+var	triangle2Area = getTriangleArea(2, 5);
+var	triangle3Area = getTriangleArea(3, 6);
+
+    function getTriangleArea(a, h) {
+
+if (a, h <= 0) {
+		console.log('nieprawidlowe dane');
+}   else  {
+		return a*h/2
+}
+}
+console.log( getTriangleArea(5, -10) )
+console.log( getTriangleArea(2, 5) )
+console.log( getTriangleArea(3, 6) )
+
+
+
